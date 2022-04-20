852f593851d53f38cfba73ae3d6f4d7e171f45cd
Zahra Ebrahim zade <eb.zahra99@gmail.com>
2022-04-21 00:05:31 +0430

    account js added

---
+++ js/account.js
@@ -0,0 +1,11 @@
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+var triggerEl = document.querySelector('#myTab a[href="#profile"]')
+bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
